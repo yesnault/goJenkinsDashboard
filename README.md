227407631cc6fe86db7374d8ba829e40804544dc
Yvonnick Esnault <yvonnick.esnault@gmail.com>
2015-03-16 23:08:23 +0100

    feat(filter) : add filter on jobs

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Draft - WIP
 ```
 go build && ./goJenkinsDashboard.go -logtostderr=true -stderrthreshold=INFO -jenkinsUrl=http://192.168.59.103:10000
 ```
+
+Filter on jobs : 
+```
+
+go build && ./goJenkinsDashboard.go -logtostderr=true -stderrthreshold=INFO -jenkinsUrl=http://192.168.59.103:10000 -filter=^job\[0-9\]$
+```
+
+see examples on filter on http://play.golang.org/p/Mpl-NThwrS
--- goJenkinsDashboard.go
+++ goJenkinsDashboard.go
@@ -6,6 +6,7 @@ import (
 	ui "github.com/gizak/termui"
 	"github.com/golang/glog"
 	tm "github.com/nsf/termbox-go"
+	"regexp"
 	"time"
 )
 
@@ -15,8 +16,9 @@ func init() {
 
 var sampleInterval = flag.Duration("interval", 5*time.Second, "Interval between sampling (default:5s)")
 var jenkinsUrl = flag.String("jenkinsUrl", "", "Jenkins Url")
+var filter = flag.String("filter", "", "Filter job")
 
-// TODO add new parameter to filter jobs
+var filterBuildName *regexp.Regexp
 
 func main() {
 	defer glog.Flush()
@@ -31,6 +33,10 @@ func main() {
 
 	jenkins := gojenkins.CreateJenkins(*jenkinsUrl).Init()
 	ls, p, redbox, yellowbox, greenbox := initWidgets()
+
+	if *filter != "" {
+		filterBuildName = regexp.MustCompile(*filter)
+	}
 
 	evt := make(chan tm.Event)
 	go func() {
@@ -49,13 +55,12 @@ func main() {
 		case <-ticker:
 			jenkins.Poll()
 			ls.Items = ls.Items[:0]
-			// TODO remove reset to avoid blink
 			resetBox(redbox, yellowbox, greenbox)
 			for _, k := range jenkins.GetAllJobs() {
 				addJob(ls, k, redbox, yellowbox, greenbox)
-				computeSizes(ls, redbox, yellowbox, greenbox)
-				ui.Render(ls, p, redbox, yellowbox, greenbox)
 			}
+			computeSizes(ls, redbox, yellowbox, greenbox)
+			ui.Render(ls, p, redbox, yellowbox, greenbox)
 		}
 	}
 }
@@ -67,35 +72,41 @@ func resetBox(redbox *ui.Par, yellowbox *ui.Par, greenbox *ui.Par) {
 }
 
 func addJob(list *ui.List, job *gojenkins.Job, redbox *ui.Par, yellowbox *ui.Par, greenbox *ui.Par) {
-	str := job.GetName() + " " + job.GetLastBuild().GetResult()
-	switch job.GetLastBuild().GetResult() {
-	case "SUCCESS":
-		greenbox.BgColor = ui.ColorGreen
-	case "WARNING":
-		yellowbox.BgColor = ui.ColorYellow
-	case "FAILURE":
-		redbox.BgColor = ui.ColorRed
+	if filterBuildName != nil && filterBuildName.MatchString(job.GetName()) {
+		str := job.GetName()
+		if job.GetLastBuild() != nil {
+			str += " " + " " + job.GetLastBuild().GetResult()
+			switch job.GetLastBuild().GetResult() {
+			case "SUCCESS":
+				greenbox.BgColor = ui.ColorGreen
+			case "WARNING":
+				yellowbox.BgColor = ui.ColorYellow
+			case "FAILURE":
+				redbox.BgColor = ui.ColorRed
+			}
+		}
+
+		list.Items = append(list.Items, str)
 
 	}
-	list.Items = append(list.Items, str)
 }
 
 func computeSizes(list *ui.List, redbox *ui.Par, yellowbox *ui.Par, greenbox *ui.Par) {
 	w, h := tm.Size()
-	list.Width = int(w * 75 / 100)
+	list.Width = w - 15
 	list.Height = h - 3
 
-	redbox.Height = 4
+	redbox.Height = 5
 	redbox.Width = 15
 	redbox.X = w - 15
 	redbox.Y = 3
 
-	yellowbox.Height = 4
+	yellowbox.Height = 5
 	yellowbox.Width = 15
 	yellowbox.X = w - 15
 	yellowbox.Y = 8
 
-	greenbox.Height = 4
+	greenbox.Height = 5
 	greenbox.Width = 15
 	greenbox.X = w - 15
 	greenbox.Y = 13
@@ -107,7 +118,11 @@ func computeSizes(list *ui.List, redbox *ui.Par, yellowbox *ui.Par, greenbox *ui
 func initWidgets() (*ui.List, *ui.Par, *ui.Par, *ui.Par, *ui.Par) {
 	ui.UseTheme("Jenkins Term UI")
 
-	p := ui.NewPar("q to quit - http://192.168.59.103:10000")
+	title := "q to quit - http://192.168.59.103:10000"
+	if *filter != "" {
+		title += " filter on " + *filter
+	}
+	p := ui.NewPar(title)
 	w, _ := tm.Size()
 	p.Height = 3
 	p.Width = w
